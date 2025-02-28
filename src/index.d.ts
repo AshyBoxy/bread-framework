2d7799e27ac31984c8761f24a86c1f9a50d54584
AshyBoxy <ashyboxy@ashyboxy.xyz>
2025-02-28 01:52:24 +0000

    rename BreadDB to LevelDB

--- src/index.d.ts
+++ src/index.d.ts
@@ -9,7 +9,7 @@
 // Classes
 export { default as BreadClient, default as Client } from "./Classes/Client";
 
-export { default as BreadDB } from "./Classes/BreadDB";
+export { default as LevelDB } from "./Classes/LevelDB";
 
 export { default as Command } from "./Classes/Command";
 export { default as GuildCommand } from "./Classes/GuildCommand";
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // Classes
 export { default as BreadClient, default as Client } from "./Classes/Client";
 
-export { default as BreadDB } from "./Classes/BreadDB";
+export { default as LevelDB } from "./Classes/LevelDB";
 
 export { default as Command } from "./Classes/Command";
 export { default as GuildCommand } from "./Classes/GuildCommand";
