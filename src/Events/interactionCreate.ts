4c66d5a05784b6bf2abf1111ba1a07cd1f16599f
AshyBoxy <ashyboxy@ashyboxy.xyz>
2025-02-28 01:57:21 +0000

    call out the user who pressed the button

--- src/Events/interactionCreate.ts
+++ src/Events/interactionCreate.ts
@@ -3,9 +3,11 @@ import EventHandler from "../Classes/EventHandler";
 
 export default new EventHandler("interactionCreate", (bot) => (int): void => {
     if (int.isButton())
-        int.reply("i don't know how to handle buttons");
+        int.reply(`i don't know how to handle buttons (${int.user.displayName} pressed it)`);
     else if (int.isChatInputCommand()) {
         // int.reply("how do commands work");
+        // TODO: just this entirely, but also commandByName
+        // although this should probably search on something slightly different
         let command;
         const cmd = int.commandName.toLowerCase();
         if (bot.commands.get(cmd)) command = bot.commands.get(cmd);
