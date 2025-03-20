ecfd3892108bf7f7efda6fdf580eded79262bece
AshyBoxy <ashyboxy@ashyboxy.xyz>
2025-03-20 19:39:36 +0000

    index.d.ts exports

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,23 +4,11 @@
 // i forgot why this is the lazy method
 // and what the not lazy method is
 
-// export everything at some point
-
-// Classes
-export { default as BreadClient, default as Client } from "./Classes/Client";
-
-export { default as LevelDB } from "./Classes/LevelDB";
-
-export { default as Command } from "./Classes/Command";
-export { default as GuildCommand } from "./Classes/GuildCommand";
-
-export { default as EventHandler } from "./Classes/EventHandler";
-
 // Interfaces
 export { default as IConfig, ClientConfig } from "./Interfaces/Config";
 export { default as IGuildConfig } from "./Interfaces/GuildConfig";
 export { default as IModule } from "./Interfaces/Module";
 export { default as BreadMessage, default as Message } from "./Interfaces/Message";
+export { default as Database } from "./Interfaces/Database";
 
-// why don't i use this for classes?
 export * from "./";
