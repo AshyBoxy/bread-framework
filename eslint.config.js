9a7b3d9dbb1ddb084e43614f3a1d94f8a15a15b5
AshyBoxy <ashyboxy@ashyboxy.xyz>
2025-11-13 03:34:15 +0000

    change eslint array bracket newline to consistent

--- eslint.config.js
+++ eslint.config.js
@@ -57,9 +57,7 @@ export default [
             yoda: "error",
 
             "array-bracket-newline": [
-                "error", {
-                    minItems: 2
-                }
+                "error", "consistent"
             ],
 
             camelcase: "error",
