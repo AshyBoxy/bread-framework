1ce06f749136244bc928c08e1435d87d5848b493
AshyBoxy <ashyboxy@ashyboxy.xyz>
2025-11-13 03:30:55 +0000

    export randomnumber as random from utils

--- src/Utils/index.ts
+++ src/Utils/index.ts
@@ -1,6 +1,7 @@
 export { default as logger } from "./logger";
 
 export { default as randomNum, randomNumber, randomFloat, randomInt } from "./randomNumber";
+export * as random from "./randomNumber";
 
 export * as discord from "./discord";
 export * as mentions from "./mentions";
